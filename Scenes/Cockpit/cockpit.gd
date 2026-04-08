9e881b2ae18b826b4729e7808fd0aa5f4d6bb457
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-04-08 11:31:15 +0200

    working playground

--- Scenes/Cockpit/cockpit.gd
+++ Scenes/Cockpit/cockpit.gd
@@ -1,22 +1,71 @@
 extends Node2D
 
+signal player_a_left_speed_updated(normalized_speed: float)
+signal player_a_right_speed_updated(normalized_speed: float)
+signal player_b_left_speed_updated(normalized_speed: float)
+signal player_b_right_speed_updated(normalized_speed: float)
+
 @onready var ergo_ski: Node2D = $ErgoSki
 @onready var speedometer_a_left: Node2D = $SpeedometerALeft
 @onready var speedometer_a_right: Node2D = $SpeedometerARight
 @onready var speedometer_b_left: Node2D = $SpeedometerBLeft
 @onready var speedometer_b_right: Node2D = $SpeedometerBRight
 
+@onready var current_scene: Container = $CurrentScene
+var loaded_scene: Node2D
+
 func _ready() -> void:
     ergo_ski.PlayerSpeedUpdated.connect(_on_player_speed_updated)
 
+    set_playground("res://Scenes/Playground/playground.tscn")
+
+func _set_scene(scene_path: String) -> void:
+    if loaded_scene != null:
+        loaded_scene.queue_free()
+        loaded_scene = null
+        
+    var scene = load(scene_path).instantiate()
+    current_scene.add_child(scene)
+    loaded_scene = scene
+
+func set_playground(scene_path: String) -> void:
+    _set_scene(scene_path)
+    
+    var player_a = loaded_scene.get_node("PlayerA")
+    var player_b = loaded_scene.get_node("PlayerB")
+
+    if player_a == null:
+        push_error("PlayerA not found in loaded scene: %s" % scene_path)
+        return
+
+    if player_b == null:
+        push_error("PlayerB not found in loaded scene: %s" % scene_path)
+        return
+
+    if not player_a is Player:
+        push_error("PlayerA is not of type Player in scene: %s" % scene_path)
+        return
+
+    if not player_b is Player:
+        push_error("PlayerB is not of type Player in scene: %s" % scene_path)
+        return
+
+    player_a_left_speed_updated.connect(player_a.set_left_engine)
+    player_a_right_speed_updated.connect(player_a.set_right_engine)
+    player_b_left_speed_updated.connect(player_b.set_left_engine)
+    player_b_right_speed_updated.connect(player_b.set_right_engine)
 
 func _on_player_speed_updated(player: String, hand: String, normalized_speed: float) -> void:
     match [player, hand]:
         ["A", "Left"]:
             speedometer_a_left.set_needle(normalized_speed)
+            player_a_left_speed_updated.emit(normalized_speed)
         ["A", "Right"]:
             speedometer_a_right.set_needle(normalized_speed)
+            player_a_right_speed_updated.emit(normalized_speed)
         ["B", "Left"]:
             speedometer_b_left.set_needle(normalized_speed)
+            player_b_left_speed_updated.emit(normalized_speed)
         ["B", "Right"]:
             speedometer_b_right.set_needle(normalized_speed)
+            player_b_right_speed_updated.emit(normalized_speed)
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,32 +1,71 @@
+@tool
 extends CharacterBody2D
 
 class_name Player
 
-@export var speed: float = 300.0
-@export var turn_speed: float = 3.0
+var speed: float = 30.0
+var turn_speed: float = 1.0
 
-var movement_input: Vector2 = Vector2.ZERO
-var turn_input: float = 0
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
-func _process(_delta: float) -> void:
-    movement_input = Vector2.ZERO
-    turn_input = 0
+@export var texture: Texture2D:
+    set(value):
+        texture = value
+        _update_sprite_texture()
 
-    if Input.is_action_pressed("ui_up"):
-        movement_input = Vector2.UP
+var _left_engine_power: float = 0.0
+var _right_engine_power: float = 0.0
 
-    if Input.is_action_pressed("ui_right"):
-        turn_input += 1
+var left_engine_power: float:
+    get:
+        return _left_engine_power
 
-    if Input.is_action_pressed("ui_left"):
-        turn_input -= 1
+var right_engine_power: float:
+    get:
+        return _right_engine_power
+
+func _ready() -> void:
+    _update_sprite_texture()
+
+func set_left_engine(power: float) -> void:
+    _left_engine_power = clamp(power, 0.0, 1.0)
+
+func set_right_engine(power: float) -> void:
+    _right_engine_power = clamp(power, 0.0, 1.0)
+
+func _update_sprite_texture() -> void:
+    if sprite_2d != null and texture != null:
+        sprite_2d.texture = texture
+
+# NOTE (mristin):
+# We leave this function here for manual debugging of the behavior.
+# Usually, you can call it from _process.
+func react_to_keys(_delta: float) -> void:
+    if Input.is_key_pressed(KEY_Q):
+        set_left_engine(1.0)
+    elif Input.is_key_pressed(KEY_A):
+        set_left_engine(0.5)
+    elif Input.is_key_pressed(KEY_Z):
+        set_left_engine(0.0)
+
+    if Input.is_key_pressed(KEY_W):
+        set_right_engine(1.0)
+    elif Input.is_key_pressed(KEY_S):
+        set_right_engine(0.5)
+    elif Input.is_key_pressed(KEY_X):
+        set_right_engine(0.0)
 
 func _physics_process(delta: float) -> void:
-    if turn_input != 0:
-        rotation += turn_input * turn_speed * delta
+    var engine_average = (_left_engine_power + _right_engine_power) * 0.5
+    var engine_difference = _right_engine_power - _left_engine_power
 
-    if movement_input != Vector2.ZERO:
-        velocity = movement_input.rotated(rotation) * speed
+    # The turning is based on engine difference.
+    if engine_difference != 0:
+        rotation += engine_difference * turn_speed * delta
+
+    # The forward movement is based on average engine power.
+    if engine_average > 0:
+        velocity = Vector2.UP.rotated(rotation) * speed * engine_average
     else:
         velocity = Vector2.ZERO
 
