b4b0ebca8da5a43f63cf7b31ad9c107c83a85bc9
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-04-12 22:06:51 +0200

    laser gate wip

---
+++ Scenes/Obstacles/LaserGate/laser_gate.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# TODO: include a shader which is drawn when the gate is on.
+
+# TODO: include the timer for switching between the lights.
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
